d9d1469a6306b3a2400eb852faddf19bfad81a07
Maksym Kharchenko <charchenkomax.new@protonmail.com>
2023-10-26 02:36:20 +0200

    dodanie szkicu interfejsu wypisującego informację o wykrytym samochodzie

--- gui.py
+++ gui.py
@@ -1,16 +1,67 @@
 import sys
 import cv2
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QGridLayout, QPushButton, QFileDialog, \
-    QSizePolicy
-from PyQt5.QtGui import QImage, QPixmap, QFont
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, \
+    QLabel, QGridLayout, QPushButton, QFileDialog, QSizePolicy, QHBoxLayout
+from PyQt5.QtGui import QImage, QPixmap, QFont, QIcon
 from PyQt5.QtCore import Qt, QTimer
-# import numpy as np
+
+
+class SecondScreen(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Second Screen")
+
+        layout = QGridLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        label = QLabel()
+        label.setStyleSheet("background-color: white;")
+        layout.addWidget(label, 0, 0, 9, 16)
+
+        info_label1 = QLabel("SC 12345")
+        info_label1.setAlignment(Qt.AlignCenter)
+        info_label1.setStyleSheet("background-color: white; color: black;")
+        info_label1.setFont(QFont("Arial", 75, QFont.Bold))  # Change the font and size
+        layout.addWidget(info_label1, 0, 0, 1, 16)
+
+        ##
+
+        # Create a label
+        horizontal_label = QLabel()
+        horizontal_label.setAlignment(Qt.AlignCenter)
+
+        # Create a font with Arial 75px
+        horizontal_label.setFont(QFont("Arial", 75, QFont.Bold))  # Change the font and size
+
+        # Set the text for the label
+        horizontal_label.setText("Your Text")
+
+        original_icon = QIcon('car.png')
+        icon_label = QLabel()
+        icon_label.setPixmap(original_icon.pixmap(400, 400))
+
+        # Create a horizontal layout for the icon and text
+        h_layout = QHBoxLayout()
+        h_layout.addWidget(icon_label)
+        h_layout.addWidget(horizontal_label)
+
+        # Add some margin to the icon and text
+        h_layout.setContentsMargins(20, 20, 20, 20)
+
+        # Add the horizontal layout to the grid layout
+        layout.addLayout(h_layout, 1, 2, 1, 14)
+
+        ##
+
+        self.setLayout(layout)
 
 
 class FullScreenApp(QMainWindow):
 
     def __init__(self, hide_button):
         super().__init__()
+
+        self.second_screen = SecondScreen()
 
         self.setWindowTitle("FullScreenApp")
 
@@ -19,13 +70,9 @@ class FullScreenApp(QMainWindow):
 
         self.video_widget = QLabel()  # Use a QLabel to display the video
         self.video_widget.setStyleSheet("background-color: black;")
-        video_layout = QGridLayout()
-        video_layout.setContentsMargins(0, 0, 0, 0)
-        self.video_widget.setLayout(video_layout)
 
         central_layout = QGridLayout()
         central_layout.setContentsMargins(0, 0, 0, 0)
-        # central_layout.addWidget(self.video_widget, 2, 0, 7, 14)
         central_layout.addWidget(self.video_widget, 1, 0, 8, 16)
 
         info_label1 = QLabel("PARKING AUTOMATYCZNY")
@@ -52,9 +99,14 @@ class FullScreenApp(QMainWindow):
             self.cap = None  # Initialize the video capture object as None
 
         # Create a timer to update the video frame
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.update_frame)
-        self.timer.start(30)  # Update every 30 milliseconds
+        # self.timer = QTimer(self)
+        # self.timer.timeout.connect(self.update_frame)
+        # self.timer.start(30)  # Update every 30 milliseconds
+
+        self.open_second_screen()
+
+    def open_second_screen(self):
+        self.setCentralWidget(self.second_screen)
 
     def update_frame(self):
         if self.cap is not None:
