fe7e848e9ea0e645ef92a38495f5aeae17228d18
Maksym Kharchenko <charchenkomax.new@protonmail.com>
2023-11-03 15:40:02 +0100

    aktualizacja ekranu wjazdowego

--- gui.py
+++ gui.py
@@ -1,7 +1,10 @@
 import sys
+
+from PyQt5 import QtCore
+
 import cv2
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, \
-    QLabel, QGridLayout, QPushButton, QFileDialog, QSizePolicy, QHBoxLayout
+    QLabel, QGridLayout, QPushButton, QFileDialog, QSizePolicy, QHBoxLayout, QFrame
 from PyQt5.QtGui import QImage, QPixmap, QFont, QIcon
 from PyQt5.QtCore import Qt, QTimer
 
@@ -9,47 +12,51 @@ from PyQt5.QtCore import Qt, QTimer
 class SecondScreen(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Second Screen")
+        self.setWindowTitle("Ekran wjazdowy")
+
+        # dodanie czarnego tłą dla spacingu
+        self.setAutoFillBackground(True)
+        p = self.palette()
+        p.setColor(self.backgroundRole(), Qt.black)
+        self.setPalette(p)
 
         layout = QGridLayout()
+        layout.setSpacing(5)
+
         layout.setContentsMargins(0, 0, 0, 0)
 
-        label = QLabel()
-        label.setStyleSheet("background-color: white;")
-        layout.addWidget(label, 0, 0, 9, 16)
+        info_label = QLabel()
+        info_label.setStyleSheet("background-color: white;")
+        layout.addWidget(info_label, 1, 0, 8, 16)
 
-        info_label1 = QLabel("SC 12345")
-        info_label1.setAlignment(Qt.AlignCenter)
-        info_label1.setStyleSheet("background-color: white; color: black;")
-        info_label1.setFont(QFont("Arial", 75, QFont.Bold))  # Change the font and size
-        layout.addWidget(info_label1, 0, 0, 1, 16)
+        license_plate_label = QLabel("SC 12345")
+        license_plate_label.setAlignment(Qt.AlignCenter)
+        license_plate_label.setStyleSheet("background-color: white; color: black;")
+        license_plate_label.setFont(QFont("Arial", 75, QFont.Bold))
+        layout.addWidget(license_plate_label, 0, 0, 1, 16)
 
         ##
 
-        # Create a label
-        horizontal_label = QLabel()
-        horizontal_label.setAlignment(Qt.AlignCenter)
+        vehicle_type_label = QLabel()
+        vehicle_type_label.setAlignment(QtCore.Qt.AlignmentFlag.AlignVCenter | QtCore.Qt.AlignmentFlag.AlignLeft)
+        vehicle_type_label.setFont(QFont("Arial", 75, QFont.Bold))
+        vehicle_type_label.setText("Samochód")
+        vehicle_type_label.setContentsMargins(20, 20, 20, 20)
 
-        # Create a font with Arial 75px
-        horizontal_label.setFont(QFont("Arial", 75, QFont.Bold))  # Change the font and size
-
-        # Set the text for the label
-        horizontal_label.setText("Your Text")
-
-        original_icon = QIcon('car.png')
-        icon_label = QLabel()
-        icon_label.setPixmap(original_icon.pixmap(400, 400))
+        vehicle_original_icon = QIcon('resources/images/car.png')
+        vehicle_icon_label = QLabel()
+        vehicle_icon_label.setPixmap(vehicle_original_icon.pixmap(300, 300))
+        vehicle_icon_label.setAlignment(QtCore.Qt.AlignmentFlag.AlignVCenter | QtCore.Qt.AlignmentFlag.AlignRight)
+        vehicle_icon_label.setContentsMargins(20, 20, 20, 20)
 
         # Create a horizontal layout for the icon and text
         h_layout = QHBoxLayout()
-        h_layout.addWidget(icon_label)
-        h_layout.addWidget(horizontal_label)
-
-        # Add some margin to the icon and text
-        h_layout.setContentsMargins(20, 20, 20, 20)
+        h_layout.addWidget(vehicle_icon_label)
+        h_layout.addWidget(vehicle_type_label)
+        h_layout.setAlignment(Qt.AlignCenter)
 
         # Add the horizontal layout to the grid layout
-        layout.addLayout(h_layout, 1, 2, 1, 14)
+        layout.addLayout(h_layout, 1, 4, 1, 7)
 
         ##
 
@@ -69,9 +76,10 @@ class FullScreenApp(QMainWindow):
         self.setCentralWidget(central_widget)
 
         self.video_widget = QLabel()  # Use a QLabel to display the video
-        self.video_widget.setStyleSheet("background-color: black;")
+        self.video_widget.setStyleSheet("background-color: black")
 
         central_layout = QGridLayout()
+        central_layout.setSpacing(5)
         central_layout.setContentsMargins(0, 0, 0, 0)
         central_layout.addWidget(self.video_widget, 1, 0, 8, 16)
 
