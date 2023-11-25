2aab01ad6b12819e79b0d5cc7d926a4e110a7985
Maksym Kharchenko <charchenkomax.new@protonmail.com>
2023-11-25 23:45:29 +0100

    aktualizacja wyświetlania schematu parkingu

--- gui/screen_location.py
+++ gui/screen_location.py
@@ -3,12 +3,9 @@ from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QVBoxLayout, QLa
 from PyQt5.QtGui import QPainter, QColor, QFont, QPixmap, QPen, QIcon
 from PyQt5.QtCore import Qt
 
-# Assuming you have fetched data using psycopg2 from the 'Sectors' table
-# Sample data for demonstration purposes
 sectors_data = [
     {'sector_id': 1, 'sector_name': 'Sector A', 'row': 0, 'col': 0, 'row_span': 150, 'column_span': 200},
     {'sector_id': 2, 'sector_name': 'Sector B', 'row': 750, 'col': 900, 'row_span': 150, 'column_span': 300},
-    # Add more sector data here...
 ]
 
 
@@ -17,13 +14,13 @@ class LocationHelpScreen(QWidget):
     def generate_button(text: str, img_path: str):
         original_icon = QIcon(img_path)
         icon_label = QLabel()
-        icon_label.setPixmap(original_icon.pixmap(75, 75))
+        icon_label.setPixmap(original_icon.pixmap(125, 125))
         icon_label.setAlignment(Qt.AlignCenter)
         icon_label.setContentsMargins(5, 5, 5, 5)
 
         text_label = QLabel()
         text_label.setAlignment(Qt.AlignCenter)
-        text_label.setFont(QFont("Arial", 30, QFont.Bold))
+        text_label.setFont(QFont("Arial", 50, QFont.Bold))
         text_label.setText(text)
         text_label.setContentsMargins(5, 5, 5, 5)
 
@@ -57,9 +54,8 @@ class LocationHelpScreen(QWidget):
 
         parking_scheme_layout = QGridLayout(self)
         parking_scheme_layout.addWidget(white_fullscreen_label, 0, 0, 900, 1200)
-        parking_scheme_layout.setContentsMargins(10, 10, 10, 10)  # Adjust margins as needed
+        parking_scheme_layout.setContentsMargins(10, 10, 10, 10)
 
-        # Add sectors dynamically based on fetched data
         for sector in sectors_data:
             sector_widget = QWidget()
             sector_layout = QVBoxLayout(sector_widget)
@@ -67,13 +63,13 @@ class LocationHelpScreen(QWidget):
 
             vehicle_original_icon = QIcon(f'gui/resources/images/car_2.png')
             vehicle_icon_label = QLabel()
-            vehicle_icon_label.setPixmap(vehicle_original_icon.pixmap(100, 100))
+            vehicle_icon_label.setPixmap(vehicle_original_icon.pixmap(120, 120))
             vehicle_icon_label.setAlignment(Qt.AlignCenter)
             vehicle_icon_label.setContentsMargins(5, 5, 5, 5)
             vehicle_icon_label.setStyleSheet("border-width: 0px;")
 
             sector_name_label = QLabel(sector['sector_name'])
-            sector_name_label.setFont(QFont("Arial", 30, QFont.Bold))
+            sector_name_label.setFont(QFont("Arial", 40, QFont.Bold))
             sector_name_label.setStyleSheet("border-width: 0px;")
 
             sector_layout.addWidget(vehicle_icon_label)
@@ -81,7 +77,6 @@ class LocationHelpScreen(QWidget):
 
             sector_widget.setStyleSheet("border: 5px solid black;")
 
-            # Highlight the specified sector
             if sector['sector_id'] == self.highlight_sector_id:
                 sector_widget.setStyleSheet("border: 5px solid black;background-color: yellow;")
 
@@ -92,9 +87,5 @@ class LocationHelpScreen(QWidget):
         parking_scheme_widget.setLayout(parking_scheme_layout)
         main_layout.addWidget(parking_scheme_widget, 0, 4, 9, 12)
 
-        # back_button = QPushButton('Back')
         back_button = self.generate_button('Powrót', 'gui/resources/images/back.png')
-        # back_button.setStyleSheet("background-color: black; color: white;")
-        # back_button.setFont(QFont("Arial", 10, QFont.Bold))
-        # back_button.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
         main_layout.addWidget(back_button, 3, 0, 3, 4)  # Row 2 with row_span 2
